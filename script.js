aadf04f78825ef343d774af33e5eefcc3bcf1b3c
MVB1512 <85173710+MVB1512@users.noreply.github.com>
2023-02-05 23:59:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+//Seleccion del nav
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    //quitar menu
+    //en modo responsive
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+//mostrar menu tres rayitas
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
